4f64a9d90689a85310d8ab2207e8dd55217587ff
fbmnds <fbmnds7@gmail.com>
2013-08-04 10:41:33 +0200

    non-linear convection, tests OK

--- src/cfd_clojure.clj
+++ src/cfd_clojure.clj
@@ -3,19 +3,10 @@
 
 
 
-;;(defn u_i [u_ni u_ni-1 c dx dt]
-;;  (- u_ni (/ (* c dt (- u_ni u_ni-1)) dx)))
-;;
-;;
-;;
-;;(defn linear-convection [u0 c nx dx nt dt]
-;;  (let [u0_0 (first u0)]
-;;    (loop [u u0
-;;           n 0]
-;;      (if (= n nt)
-;;        u
-;;        (recur (conj (map #(u_i (second %) (first %) c dx dt) (partition 2 1 u)) u0_0)
-;;               (inc n))))))
+
+(defn discretize [f m u]
+  (let [g (fn [u] (conj (map #(f m %) (partition (:x-steps m) 1 u)) (first u)))]
+    (iterate g u)))
 
 
 
@@ -24,14 +15,10 @@
 
 
 
-;;(defn discretize
-;;  ([f m u]
-;;     (let [r (conj u (conj (map #(f m %) (partition {:x-steps m} 1 u)) (first u)))]
-;;       (lazy-seq (cons (discretize f m (last r) r)))
-;;  ([f m u c]
-;;     (let [r (conj (map #(f m %) (partition {:x-steps m} 1 u)) (first u))]
-;;       (lazy-seq (cons (discretize f m r c) c)))))))
+(defn non-linear-convection [m [u_ni-1 u_ni]]
+  (- u_ni (/ (* u_ni (:dt m) (- u_ni u_ni-1)) (:dx m))))
 
-(defn discretize [f m u]
-  (let [g (fn [u] (conj (map #(f m %) (partition (:x-steps m) 1 u)) (first u)))]
-    (iterate g u)))
+
+
+(defn diffusion-1D [m [u_ni-1 u_ni u_ni+1]]
+  (+ u_ni (/ (* (:nu m) (:dt m) (+ u_ni+1 u_ni-1 (* -2. u_ni))) (Math/pow (:dx m) 2.))))
---
+++ test/cfd_clojure/python/lesson-02.py
@@ -0,0 +1,61 @@
+# Remember: comments in python are denoted by the pound sign
+import numpy as np                 #here we load numpy, calling it 'np' from now on
+import matplotlib.pyplot as plt    #here we load matplotlib, calling it 'plt'
+import time, sys                   #and load some utilities
+from IPython.core.display import clear_output #used for inline animation
+
+nx = 41  # try changing this number from 41 to 81 and Run All ... what happens?
+dx = 2./(nx-1)
+nt = 20    #nt is the number of timesteps we want to calculate
+dt = .025  #dt is the amount of time each timestep covers (delta t)
+
+
+u = np.ones(nx)      #numpy function ones()
+u[.5/dx : 1/dx+1]=2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+print (nx, dx, nt, dt)
+print (u)
+
+plt.plot(np.linspace(0,2,nx), u)
+plt.show()
+
+un = np.ones(nx) #initialize a temporary array
+
+for n in range(nt):  #loop for values of n from 0 to nt, so it will run nt times
+    un[:] = u[:] ##copy the existing values of u into un
+    #for i in range(1,nx): ## you can try commenting this line and...
+    for i in range(nx): ## ... uncommenting this line and see what happens!
+        u[i] = un[i]-un[i]*dt/dx*(un[i]-un[i-1])
+
+print (u)
+
+plt.plot(np.linspace(0,2,nx),u)
+plt.show()
+
+
+nx = 81  # try changing this number from 41 to 81 and Run All ... what happens?
+dx = 2./(nx-1)
+nt = 20    #nt is the number of timesteps we want to calculate
+dt = .001  #dt is the amount of time each timestep covers (delta t)
+
+
+u = np.ones(nx)      #numpy function ones()
+u[.5/dx : 1/dx+1]=2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+print (u)
+
+plt.plot(np.linspace(0,2,nx), u)
+plt.show()
+
+un = np.ones(nx) #initialize a temporary array
+
+for n in range(nt):  #loop for values of n from 0 to nt, so it will run nt times
+    un[:] = u[:] ##copy the existing values of u into un
+    #for i in range(1,nx): ## you can try commenting this line and...
+    for i in range(nx): ## ... uncommenting this line and see what happens!
+        u[i] = un[i]-un[i]*dt/dx*(un[i]-un[i-1])
+
+print (nx, dx, nt, dt)
+print (u)
+
+
+plt.plot(np.linspace(0,2,nx),u)
+plt.show()
