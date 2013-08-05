ad06b3b37c753f2f7f96f98f810044ede3abfac7
fbmnds <fbmnds7@gmail.com>
2013-08-05 13:11:00 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,17 @@
   :description "CFD Python port to Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
 
-                 [incanter/incanter-core "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-io "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-charts "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-mongodb "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-pdf "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-latex "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-excel "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-sql "1.5.0-SNAPSHOT"]
-                 [incanter/incanter-zoo "1.5.0-SNAPSHOT"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
+
+                 [incanter/incanter-core "1.5.2"]
+                 [incanter/incanter-io "1.5.2"]
+                 [incanter/incanter-charts "1.5.2"]
+                 ;[incanter/incanter-mongodb "1.5.2"]
+                 ;[incanter/incanter-pdf "1.5.2"]
+                 ;[incanter/incanter-latex "1.5.2"]
+                 ;[incanter/incanter-excel "1.5.2"]
+                 ;[incanter/incanter-sql "1.5.2"]
+                 ;[incanter/incanter-zoo "1.5.2"]
                  [swingrepl "1.3.0"
                   :exclusions [org.clojure/clojure
                                org.clojure/clojure-contrib]]
--- src/cfd_clojure.clj
+++ src/cfd_clojure.clj
@@ -1,7 +1,8 @@
 (ns cfd-clojure
+  (:require [clojure.math.numeric-tower :as math])
   (:use (incanter core charts)))
 
-
+;; provide discretisation linear in t and up to second order in x
 
 (defn discretize [f m u]
   (let [l (last u)
@@ -11,17 +12,22 @@
       (iterate g u)
       (iterate h u))))
 
-
+;; Step 1
 
 (defn linear-convection [m [u_ni-1 u_ni]]
   (- u_ni (/ (* (:c m) (:dt m) (- u_ni u_ni-1)) (:dx m))))
 
-
+;; Step 2
 
 (defn non-linear-convection [m [u_ni-1 u_ni]]
   (- u_ni (/ (* u_ni (:dt m) (- u_ni u_ni-1)) (:dx m))))
 
-
+;; Step 3
 
 (defn diffusion-1D [m [u_ni-1 u_ni u_ni+1]]
   (+ u_ni (/ (* (:nu m) (:dt m) (+ u_ni+1 u_ni-1 (* -2. u_ni))) (Math/pow (:dx m) 2.))))
+
+
+;; Step 4
+
+;; -2*nu*(-(-8*t + 2*x)*exp(-(-4*t + x)**2/(4*nu*(t + 1)))/(4*nu*(t + 1)) - (-8*t + 2*x - 12.5663706143592)*exp(-(-4*t + x - 6.28318530717959)**2/(4*nu*(t + 1)))/(4*nu*(t + 1)))/(exp(-(-4*t + x - 6.28318530717959)**2/(4*nu*(t + 1))) + exp(-(-4*t + x)**2/(4*nu*(t + 1)))) + 4
