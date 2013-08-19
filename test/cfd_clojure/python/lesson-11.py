b8d929eac6f2553b506f8db1d2902c00f93dbee5
fbmnds <fbmnds7@gmail.com>
2013-08-19 11:41:26 +0200

    test cases for lesson 11

--- test/cfd_clojure/python/lesson-11.py
+++ test/cfd_clojure/python/lesson-11.py
@@ -3,25 +3,30 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 
-nx = 41
-ny = 41
-nt = 500
-nit=50
-c = 1
-dx = 2.0/(nx-1)
-dy = 2.0/(ny-1)
-x = np.linspace(0,2,nx)
-y = np.linspace(0,2,ny)
-Y,X = np.meshgrid(y,x)
 
-rho = 1
-nu = .1
-dt = .001
+###
 
-u = np.zeros((ny, nx))
-v = np.zeros((ny, nx))
-p = np.zeros((ny, nx))
-b = np.zeros((ny, nx))
+def print_u (f, label1, u, ny, nx, label2):
+    f.write(label1)
+    for j in range(ny):
+        f.write("[")
+        for i in range(nx):
+            f.write(str(u[j,i]))
+            if i < nx-1:
+                f.write(", ")
+        f.write("]\n")
+    f.write(label2)
+
+def print_v (f, label1, v, n, label2):
+    f.write(label1)
+    for i in range(n):
+        f.write(str(v[i]))
+        if i < n-1:
+            f.write(", ")
+    f.write(label2)
+
+###
+
 
 
 def buildUpB(b, rho, dt, u, v, dx, dy):
@@ -33,6 +38,8 @@ def buildUpB(b, rho, dt, u, v, dx, dy):
 
     return b
 
+
+nit=50  ## implicite parameter of presPoisson
 
 def presPoisson(p, dx, dy, b):
     pn = np.empty_like(p)
@@ -87,16 +94,79 @@ def cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu):
         v[:,-1] = 0
         u[-1,:] = 0
 
-    return u, v, p
+    return u, v, p, b
 
 
+
+
+nx = 41
+ny = 41
+nt = 500   ## subsequently overwritten
+## nit=50  ## implicite parameter of presPoisson
+c = 1      ## unused
+dx = 2.0/(nx-1)
+dy = 2.0/(ny-1)
+x = np.linspace(0,2,nx)
+y = np.linspace(0,2,ny)
+Y,X = np.meshgrid(y,x)
+
+rho = 1
+nu = .1
+dt = .001
+
+# u = np.zeros((ny, nx))
+# v = np.zeros((ny, nx))
+# p = np.zeros((ny, nx))
+# b = np.zeros((ny, nx))
 
 u = np.zeros((ny, nx))
 v = np.zeros((ny, nx))
 p = np.zeros((ny, nx))
 b = np.zeros((ny, nx))
-nt = 200
-u, v, p = cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu)
+nt = 200  ## variable test case parameter
+
+### first test case
+
+f = open(''.join(['test-11-', str(nt), '.json']),'w')
+
+f.write("{ \"nx\" : ")
+f.write(str(nx))
+f.write(", \"dx\" : ")
+f.write(str(dx))
+
+f.write(", \"ny\" : ")
+f.write(str(ny))
+f.write(", \"dy\" : ")
+f.write(str(dy))
+
+f.write(", \"nt\" : ")
+f.write(str(nt))
+f.write(", \"dt\" : ")
+f.write(str(dt))
+
+f.write(", \"rho\" : ")
+f.write(str(rho))
+f.write(", \"nu\" : ")
+f.write(str(nu))
+
+f.write(", \"nit\" : ")
+f.write(str(nit))
+
+print_u (f, ",\n \"p0\" : [", p, nx, ny, "]")
+print_u (f, ",\n \"b0\" : [", b, nx, ny, "]")
+print_u (f, ",\n \"u0\" : [", u, nx, ny, "]")
+print_u (f, ",\n \"v0\" : [", v, nx, ny, "]")
+
+
+u, v, p, b = cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu)
+
+print_u (f,''.join([",\n \"p_", str(nt),"\" : ["]), p, nx, ny, "]")
+print_u (f,''.join([",\n \"b_", str(nt),"\" : ["]), b, nx, ny, "]")
+print_u (f,''.join([",\n \"u_", str(nt),"\" : ["]), u, nx, ny, "]")
+print_u (f,''.join([",\n \"v_", str(nt),"\" : ["]), v, nx, ny, "] }\n")
+
+f.close()
+
 fig = plt.figure(figsize=(11,7), dpi=100)
 plt.contourf(X,Y,p,alpha=0.5)    ###plnttong the pressure field as a contour
 plt.colorbar()
@@ -105,15 +175,131 @@ plt.quiver(X[::2,::2],Y[::2,::2],u[::2,::2],v[::2,::2]) ##plotting velocity
 plt.xlabel('X')
 plt.ylabel('Y')
 fig.show()
-input(" press ENTER to continue ")
+#input(" press ENTER to continue ")
 
+
+### second test case
+
+###  reinitialization
 
 u = np.zeros((ny, nx))
 v = np.zeros((ny, nx))
 p = np.zeros((ny, nx))
 b = np.zeros((ny, nx))
-nt = 700
-u, v, p = cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu)
+nt = 700  ## variable test case parameter
+
+f = open(''.join(['test-11-', str(nt), '.json']),'w')
+
+f.write("{ \"nx\" : ")
+f.write(str(nx))
+f.write(", \"dx\" : ")
+f.write(str(dx))
+
+f.write(", \"ny\" : ")
+f.write(str(ny))
+f.write(", \"dy\" : ")
+f.write(str(dy))
+
+f.write(", \"nt\" : ")
+f.write(str(nt))
+f.write(", \"dt\" : ")
+f.write(str(dt))
+
+f.write(", \"rho\" : ")
+f.write(str(rho))
+f.write(", \"nu\" : ")
+f.write(str(nu))
+
+f.write(", \"nit\" : ")
+f.write(str(nit))
+
+print_u (f, ",\n \"p0\" : [", p, nx, ny, "]")
+print_u (f, ",\n \"b0\" : [", b, nx, ny, "]")
+print_u (f, ",\n \"u0\" : [", u, nx, ny, "]")
+print_u (f, ",\n \"v0\" : [", v, nx, ny, "]")
+
+
+u, v, p, b = cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu)
+
+print_u (f,''.join([",\n \"p_", str(nt),"\" : ["]), p, nx, ny, "]")
+print_u (f,''.join([",\n \"b_", str(nt),"\" : ["]), b, nx, ny, "]")
+print_u (f,''.join([",\n \"u_", str(nt),"\" : ["]), u, nx, ny, "]")
+print_u (f,''.join([",\n \"v_", str(nt),"\" : ["]), v, nx, ny, "] }\n")
+
+f.close()
+
+
+fig = plt.figure(figsize=(11,7), dpi=100)
+plt.contourf(X,Y,p,alpha=0.5)
+plt.colorbar()
+plt.contour(X,Y,p)
+plt.quiver(X[::2,::2],Y[::2,::2],u[::2,::2],v[::2,::2])
+plt.xlabel('X')
+plt.ylabel('Y')
+fig.show()
+#input(" press ENTER to continue ")
+
+
+### third test case
+
+###  reinitialization
+
+nx = 10
+ny = 10
+
+dx = 2.0/(nx-1)
+dy = 2.0/(ny-1)
+x = np.linspace(0,2,nx)
+y = np.linspace(0,2,ny)
+Y,X = np.meshgrid(y,x)
+
+u = np.zeros((ny, nx))
+v = np.zeros((ny, nx))
+p = np.zeros((ny, nx))
+b = np.zeros((ny, nx))
+nt = 3  ## variable test case parameter
+
+f = open(''.join(['test-11-', str(nt), '.json']),'w')
+
+f.write("{ \"nx\" : ")
+f.write(str(nx))
+f.write(", \"dx\" : ")
+f.write(str(dx))
+
+f.write(", \"ny\" : ")
+f.write(str(ny))
+f.write(", \"dy\" : ")
+f.write(str(dy))
+
+f.write(", \"nt\" : ")
+f.write(str(nt))
+f.write(", \"dt\" : ")
+f.write(str(dt))
+
+f.write(", \"rho\" : ")
+f.write(str(rho))
+f.write(", \"nu\" : ")
+f.write(str(nu))
+
+f.write(", \"nit\" : ")
+f.write(str(nit))
+
+print_u (f, ",\n \"p0\" : [", p, nx, ny, "]")
+print_u (f, ",\n \"b0\" : [", b, nx, ny, "]")
+print_u (f, ",\n \"u0\" : [", u, nx, ny, "]")
+print_u (f, ",\n \"v0\" : [", v, nx, ny, "]")
+
+
+u, v, p, b = cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu)
+
+print_u (f,''.join([",\n \"p_", str(nt),"\" : ["]), p, nx, ny, "]")
+print_u (f,''.join([",\n \"b_", str(nt),"\" : ["]), b, nx, ny, "]")
+print_u (f,''.join([",\n \"u_", str(nt),"\" : ["]), u, nx, ny, "]")
+print_u (f,''.join([",\n \"v_", str(nt),"\" : ["]), v, nx, ny, "] }\n")
+
+f.close()
+
+
 fig = plt.figure(figsize=(11,7), dpi=100)
 plt.contourf(X,Y,p,alpha=0.5)
 plt.colorbar()
