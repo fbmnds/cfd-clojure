f42027dceb3e22293c9a4b659b579c9d3c7f4734
fbmnds <fbmnds7@gmail.com>
2013-08-23 12:14:05 +0200

    test case for cavity flow prepared

--- test/cfd_clojure/python/lesson-11.py
+++ test/cfd_clojure/python/lesson-11.py
@@ -64,6 +64,20 @@ def results(f,nx,dx,ny,dy,dt,u,v,p,b0):
 
     f.write( "}" )
 
+
+
+def results1(f,nx,dx,ny,dy,dt,u,v,p,b):
+    print_u (f,",\n \"u_nt\" : [", u, nx, ny, "]")
+    print_u (f,",\n \"v_nt\" : [", v, nx, ny, "]")
+    print_u (f,",\n \"b_nt\" : [", b, nx, ny, "]")
+    print_u (f,",\n \"p_nt\" : [", p, nx, ny, "]")
+    p_py = presPoisson(p, dx, dy, b)
+    print_u (f,",\n \"p_py\" : [", p_py, nx, ny, "]")
+
+    f.write( "}" )
+
+
+
 ###
 
 
@@ -275,3 +289,93 @@ plt.xlabel('X')
 plt.ylabel('Y')
 fig.show()
 input(" press ENTER to continue ")
+
+
+### fourth test case
+
+
+
+def test_cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu, b):
+    un = np.empty_like(u)
+    vn = np.empty_like(v)
+    ## b = np.zeros((ny, nx))
+
+    #for n in range(nt):
+    un[:] = u[:]
+    vn[:] = v[:]
+
+    ## b = buildUpB(b, rho, dt, u, v, dx, dy)
+    ## p = presPoisson(p, dx, dy, b)
+
+    u[1:-1,1:-1] = un[1:-1,1:-1]-\
+        un[1:-1,1:-1]*dt/dx*(un[1:-1,1:-1]-un[0:-2,1:-1])-\
+        vn[1:-1,1:-1]*dt/dy*(un[1:-1,1:-1]-un[1:-1,0:-2])-\
+        dt/(2*rho*dx)*(p[2:,1:-1]-p[0:-2,1:-1])+\
+        nu*(dt/dx**2*(un[2:,1:-1]-2*un[1:-1,1:-1]+un[0:-2,1:-1])+\
+        dt/dy**2*(un[1:-1,2:]-2*un[1:-1,1:-1]+un[1:-1,0:-2]))
+
+    v[1:-1,1:-1] = vn[1:-1,1:-1]-\
+        un[1:-1,1:-1]*dt/dx*(vn[1:-1,1:-1]-vn[0:-2,1:-1])-\
+        vn[1:-1,1:-1]*dt/dy*(vn[1:-1,1:-1]-vn[1:-1,0:-2])-\
+        dt/(2*rho*dy)*(p[1:-1,2:]-p[1:-1,0:-2])+\
+        nu*(dt/dx**2*(vn[2:,1:-1]-2*vn[1:-1,1:-1]+vn[0:-2,1:-1])+\
+        (dt/dy**2*(vn[1:-1,2:]-2*vn[1:-1,1:-1]+vn[1:-1,0:-2])))
+
+    u[0,:] = 0
+    u[:,0] = 0
+    u[:,-1] = 1
+    v[0,:] = 0
+    v[-1,:]=0
+    v[:,0] = 0
+    v[:,-1] = 0
+    u[-1,:] = 0
+
+    return u, v, p, b
+
+
+
+
+###  reinitialization
+
+nx = 21
+ny = 21
+
+dx = 2.0/(nx-1)
+dy = 2.0/(ny-1)
+x = np.linspace(0,2,nx)
+y = np.linspace(0,2,ny)
+Y,X = np.meshgrid(y,x)
+
+### take u,v,p,b from third test case
+
+## u = np.zeros((ny, nx))
+## v = np.zeros((ny, nx))
+## p = np.zeros((ny, nx))
+b = np.zeros((ny, nx))  ## and reuse b0 of 3rd test case
+
+nt = 1  ## variable test case parameter / pseudo in this case
+
+f = open(''.join(['test-11-', str(nt), '.json']),'w')
+
+print_params(f,nx,dx,ny,dy,nt,dt,rho,nu,nit)
+
+print_u (f, ",\n \"p0\" : [", p, nx, ny, "]")
+print_u (f, ",\n \"u0\" : [", u, nx, ny, "]")
+print_u (f, ",\n \"v0\" : [", v, nx, ny, "]")
+print_u (f, ",\n \"b0\" : [", b0, nx, ny, "]")
+
+u, v, p, b = test_cavityFlow(nt, u, v, dt, dx, dy, p, rho, nu, b0)
+results1(f,nx,dx,ny,dy,dt,u,v,p,b)
+
+f.close()
+
+
+## fig = plt.figure(figsize=(11,7), dpi=100)
+## plt.contourf(X,Y,p,alpha=0.5)
+## plt.colorbar()
+## plt.contour(X,Y,p)
+## plt.quiver(X[::2,::2],Y[::2,::2],u[::2,::2],v[::2,::2])
+## plt.xlabel('X')
+## plt.ylabel('Y')
+## fig.show()
+## input(" press ENTER to continue ")
